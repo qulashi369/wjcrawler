71fb8a7ff58b7439601f1e0cb335d40a37cd35db
gullon <gullonlui@gmail.com>
2013-08-10 11:48:57 +0800

    add save Book item; pep8 enhance; replace books.txt with books.py

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,7 +1,7 @@
 #coding:utf8
 
 from pymongo import MongoClient
-from items import Content, Chapter
+from items import Content, Chapter, Book
 from settings import MONGO_SERVER, MONGO_PORT, MONGO_DB_NAME
 
 
@@ -12,12 +12,15 @@ class CrawlerPipeline(object):
         self.db = self.client[MONGO_DB_NAME]
         self.Chapter = self.db.Chapter
         self.Content = self.db.Content
+        self.Book = self.db.Book
 
     def process_item(self, item, spider):
         if isinstance(item, Content):
             self.save_content(item)
         elif isinstance(item, Chapter):
             self.save_chapter(item)
+        elif isinstance(item, Book):
+            self.save_book(item)
         return item
 
     def save_content(self, item):
@@ -29,3 +32,7 @@ class CrawlerPipeline(object):
         self.Chapter.insert(dict(cid=item['cid'],
                                  bid=item['bid'],
                                  title=item['title']))
+
+    def save_book(self, item):
+        self.Book.insert(dict(bid=item['bid'],
+                              title=item['title']))
