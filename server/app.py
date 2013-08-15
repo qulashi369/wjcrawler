d3032bfe083f21dc7b89438c1243ae319c7f2c85
gullon <gullonlui@gmail.com>
2013-08-16 01:03:28 +0800

    finish template & static config

--- server/app.py
+++ server/app.py
@@ -1,18 +1,21 @@
 #coding: utf8
 
 from flask import Flask
+from flask.ext.mako import MakoTemplates
+from flask.ext.mako import render_template
 
 from config import DB_URL
 from libs.db import get_db_session
 
 app = Flask(__name__)
+mako = MakoTemplates(app)
 
 db_session = get_db_session(DB_URL)
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('index.html', name=u'小伙伴')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8000)
