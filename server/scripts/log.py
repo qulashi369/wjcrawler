40130b0f91b0c64d1570049dac0b5bdf3f66f01c
yj <yj@test.com>
2013-09-01 16:35:35 +0800

    lalala

--- server/scripts/log.py
+++ server/scripts/log.py
@@ -3,8 +3,10 @@ import logging.config
 
 logging.config.fileConfig("scripts/log.conf")
 logger = logging.getLogger("Tornado")
-logger.debug('debug message')
-logger.info('info message')
-logger.warn('warn message')
-logger.error('error message')
-logger.critical('critical message')
+
+if __name__ == "__main__":
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warn('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
