d7b9a65415fe33e3150277978ef88fe37d151241
yj <yj@test.com>
2013-09-17 00:15:07 +0800

    image path

--- crawler/book/scripts/stat.py
+++ crawler/book/scripts/stat.py
@@ -1,14 +1,15 @@
 #-*-coding:utf8
 
+
 def stat():
     import pymongo
-    connection=pymongo.Connection('localhost',27017)
-    db = connection.xiaoshuo2
+    connection = pymongo.Connection('localhost', 27017)
+    db = connection.xiaoshuo_pict
     for b in db.Book.find():
-        chapter_count = db.Chapter.find({"book_id":b['bid']}).count()
-        content_count = db.Content.find({"book_id":b['bid']}).count()
-        print b['title'].encode("utf-8"), chapter_count, "\t", content_count
-    print 'book count:',db.Book.find().count()
+        chapter_count = db.Chapter.find({"book_id": b['bid']}).count()
+        content_count = db.Content.find({"book_id": b['bid']}).count()
+        print b['title'].encode("utf-8"), chapter_count, "\t", content_count, '\t', b['image_path']
+    print 'book count:', db.Book.find().count()
 
 if __name__ == "__main__":
     stat()
