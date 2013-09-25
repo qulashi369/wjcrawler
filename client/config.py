35bb137fb9d57d02ec766f7e3ccb0908b91b5313
yj <yj@test.com>
2013-09-25 21:19:22 +0800

    add config.py in client to .gitignore

--- client/config.py
+++ client/config.py
@@ -1,7 +1,7 @@
 # coding:utf8
 
-crawler_name = 'xz'
-yiwanshu = 'http://yiwanshu.com:8000'
+crawler_name = 'yj'
+yiwanshu = 'http://yiwanshu.com:80'
 
 interval = 120  # 间隔120秒拿一次更新任务
 limit = 10  # 每次拿10个更新任务
