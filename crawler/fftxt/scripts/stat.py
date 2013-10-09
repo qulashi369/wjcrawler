9a33ac102af04199e70c86ada9668a468cf0e476
guest <guest@yujie>
2013-10-09 14:36:25 +0800

    add a scripts which deletes non-chapter boos

--- crawler/fftxt/scripts/stat.py
+++ crawler/fftxt/scripts/stat.py
@@ -6,10 +6,20 @@ def stat():
     import pymongo
     connection = pymongo.Connection('localhost', 27017)
     db = connection.xiaoshuo_fftxt
+    delbooks = []
     for b in db.book.find().sort('id'):
         chapter_count = db.chapter.find({"bid": b['id']}).count()
         print b['id'], '\t', b['name'].encode("utf-8"), chapter_count, "\t"
+        if chapter_count < 20:
+            delbooks.append(b['id'])
     print 'book count:', db.book.find().count()
+    print '删除的书:', delbooks
+    delbook(db, delbooks)
+
+def delbook(db, bids):
+    for bid in bids:
+        db.book.remove({"id": bid})
+        db.chapter.remove({"bid": bid})
 
 if __name__ == "__main__":
     stat()
